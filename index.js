13e2c9992fb59a987d9c4cbdcb2b61844535116b
Sam Brozinsky <s77broz@aol.com>
2018-08-28 07:04:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name) {
-  kittens.unshift(name);
+  kittens.push(name);
 }
 
 function destructivelyRemoveLastKitten() {
